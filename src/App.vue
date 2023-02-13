7fb9d38b281ba3c88dfe23853e3be69e3fa63820
xiaoyu <1938805811@qq.com>
2023-02-13 19:31:48 +0800

    项目初始化调整初始目录

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <nav></nav>
     <router-view/>
+    <h1>黑马头条</h1>
   </div>
 </template>
 
